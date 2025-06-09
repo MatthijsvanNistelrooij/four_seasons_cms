3d565e621f1eb100947f114d82f477605b6d1ff8
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-09 09:18:36 +0200

    update confirm dialog

--- components/steps/Step6_Confirm.tsx
+++ components/steps/Step6_Confirm.tsx
@@ -36,40 +36,40 @@ export const Step6_Confirm = ({ data, onBack, onSubmit }: Props) => {
       <div className="max-w-3xl mx-auto">
         <h2 className="text-sm mb-3 text-neutral-900">Bevestig uw afspraak</h2>
 
-        <div className="rounded bg-white p-8 space-y-6">
+        <div className="rounded bg-white p-4 space-y-6">
           <dl className="grid grid-cols-1 sm:grid-cols-2 gap-x-10 gap-y-6 text-base text-neutral-800">
             <div>
               <dt className="text-sm text-neutral-500">
                 Type afspraak
               </dt>
-              <dd className="mt-1 font-medium">{service || "-"}</dd>
+              <dd className="font-medium">{service || "-"}</dd>
             </div>
             <div>
               <dt className="text-sm text-neutral-500">
                 Medewerker
               </dt>
-              <dd className="mt-1 font-medium">{barber || "-"}</dd>
+              <dd className="font-medium">{barber || "-"}</dd>
             </div>
             <div>
               <dt className="text-sm text-neutral-500">
                 Datum & tijd
               </dt>
-              <dd className="mt-1 font-medium">
+              <dd className="font-medium">
                 {date ? date.toLocaleDateString("nl-NL") : "-"} {time || "-"}
               </dd>
             </div>
             <div>
               <dt className="text-sm text-neutral-500">Naam</dt>
-              <dd className="mt-1  font-medium">{name || "-"}</dd>
+              <dd className=" font-medium">{name || "-"}</dd>
             </div>
             <div>
               <dt className="text-sm text-neutral-500">E-mail</dt>
-              <dd className="mt-1 font-medium">{email || "-"}</dd>
+              <dd className="font-medium">{email || "-"}</dd>
 
               <dt className="text-sm text-neutral-500 mt-4">
                 Telefoonnummer
               </dt>
-              <dd className="mt-1 font-medium mb-12">{phone || "-"}</dd>
+              <dd className="font-medium mb-1">{phone || "-"}</dd>
             </div>
           </dl>
         </div>
