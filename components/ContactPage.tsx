cde354c4761418ab2012e908f26e1d41df974e57
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-11 00:23:05 +0200

    updat emeail

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -81,14 +81,7 @@ const ContactPage = () => {
                 className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-pink-500"
               />
             </div>
-            <div>
-              <label className="block text-sm mb-1">Telefoonnummer</label>
-              <input
-                type="tel"
-                name="from_phone"
-                className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-pink-500"
-              />
-            </div>
+
             <div>
               <label className="block text-sm mb-1">Bericht</label>
               <textarea
