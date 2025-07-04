f5e567ad61c0b8d1d854bac7fc4a42c2d52585ed
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-07-04 02:25:23 +0200

    update about instagram embed

--- components/InstagramEmbed.tsx
+++ components/InstagramEmbed.tsx
@@ -29,28 +29,6 @@ export default function InstagramEmbed() {
             padding: "0px",
           }}
         ></blockquote>
-
-        {/* <iframe
-          className="instagram-media instagram-media-rendered"
-          id="instagram-embed-0"
-          src="https://www.instagram.com/p/DI6rnNKAjzJ/embed/captioned/?cr=1&amp;v=14&amp;wp=658&amp;rd=https%3A%2F%2Fchicago.suntimes.com&amp;rp=%2Fnews%2F2025%2F07%2F03%2Fwho-is-mello-buckzz-chicago-rapper-mass-shooting-river-north#%7B%22ci%22%3A0%2C%22os%22%3A1160.5999999642372%7D"
-
-          height="1115"
-          data-instgrm-payload-id="instagram-media-payload-0"
-          scrolling="no"
-          style={{
-            background: "white",
-            maxWidth: "658px",
-            width: "calc(100% - 2px)",
-            borderRadius: "3px",
-            border: "1px solid rgb(219, 219, 219)",
-            boxShadow: "none",
-            display: "block",
-            margin: "0px 0px 12px",
-            minWidth: "326px",
-            padding: "0px",
-          }}
-        ></iframe> */}
       </div>
     </section>
   )
