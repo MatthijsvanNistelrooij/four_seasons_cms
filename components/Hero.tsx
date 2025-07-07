e19de96f7b0f9bac06167e492210af0d0af93c81
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-07-07 07:05:24 +0200

    update hero drag

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -103,7 +103,7 @@ const Hero = ({ onOpenDialog }: HeroProps) => {
                 key={`heading-${index}`}
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.3, delay: 0.4 }}
+                transition={{ duration: 0.3, delay: 0.2 }}
                 className="text-4xl lg:text-6xl font-bold mb-2"
               >
                 {slides[index].heading}
@@ -111,9 +111,9 @@ const Hero = ({ onOpenDialog }: HeroProps) => {
 
               <motion.div
                 key={`sub-${index}`}
-                initial={{ opacity: 0, y: 25 }}
+                initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: 0.5 }}
+                transition={{ duration: 0.5, delay: 0.3}}
                 className="text-left text-xl lg:text-2xl max-w-xl"
               >
                 <h1 className="leading-[2.3rem] md:leading-[2.7rem] mt-2">
@@ -126,9 +126,9 @@ const Hero = ({ onOpenDialog }: HeroProps) => {
               <div className="mt-8 md:mt-8">
                 <motion.div
                   key={`contact-${index}`}
-                  initial={{ opacity: 0, y: 20 }}
+                  initial={{ opacity: 0, y: 30 }}
                   animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.5, delay: 0.8 }}
+                  transition={{ duration: 0.5, delay: 0.6 }}
                 >
                   <div className="flex flex-col md:flex-row gap-2">
                     <Button
