e956c15a1300f114d716ceaafcdb68c5ce018d36
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-07-07 07:06:40 +0200

    update hero drag

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -113,7 +113,7 @@ const Hero = ({ onOpenDialog }: HeroProps) => {
                 key={`sub-${index}`}
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: 0.3}}
+                transition={{ duration: 0.5, delay: 0.5}}
                 className="text-left text-xl lg:text-2xl max-w-xl"
               >
                 <h1 className="leading-[2.3rem] md:leading-[2.7rem] mt-2">
