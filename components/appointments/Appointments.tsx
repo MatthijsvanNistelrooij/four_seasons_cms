27e44314990897b55a0f856e7a6c9aab8e9ad230
Matthijs van Nistelrooy <matthijs.vannistelrooy@gmail.com>
2025-09-06 14:16:09 +0200

    update query to 100 request

--- components/appointments/Appointments.tsx
+++ components/appointments/Appointments.tsx
@@ -91,7 +91,7 @@ const Appointments = () => {
     }
   }
 
-  const itemsPerPage = 50
+  const itemsPerPage = 10
   const [currentPage, setCurrentPage] = useState(1)
 
   const totalPages = Math.ceil(filteredAppointments.length / itemsPerPage)
